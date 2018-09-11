10db49c30691680a74071d05848d6a060c0ce8ef
dozcan <dogasava@gmail.com>
2018-09-11 14:49:10 +0300

    upd

---
+++ MObilwallet/Models/WalletDetails.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NBitcoin;
+
+namespace BitcoinMobileWallet.Models
+{
+
+
+    public class WalletDetails 
+    {
+        public string _walletPath { get; set; }
+        public string _feeRequest { get; set; }
+        public string _network { get; set; }  
+
+   }
+}
