72b20b21106a5a8251583cf36e23b6b006c49cff
dozcan <dogasava@gmail.com>
2018-09-11 14:48:27 +0300

    upd

--- MObilwallet/Models/User.cs
+++ MObilwallet/Models/User.cs
@@ -1,10 +1,23 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MobilWallet.Models
+namespace BitcoinMobileWallet.Models
 {
+    public class BtcTransactionDetail
+    {
+        public string BtcSenderAddress { get; set; }
+        public string SendBtcAmount { get; set; }
+        public string BtcReceiverAddres { get; set; }
+        public User _user { get; set; }
+    }
+    public interface IUser
+    {
+         string Email { get; set; }
+         string Password { get; set; }
+    }
+
     public class User
     {
         public string Email { get; set; }
