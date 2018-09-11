207e4ffbe7b987aebf2b9112a588783e2e2ff408
dozcan <dogasava@gmail.com>
2018-09-11 14:49:40 +0300

    upd

--- MObilwallet/Models/UserTransaction.cs
+++ MObilwallet/Models/UserTransaction.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MobilWallet.Models
+namespace BitcoinMobileWallet.Models
 {
     public class BitcoinRegister
     {
